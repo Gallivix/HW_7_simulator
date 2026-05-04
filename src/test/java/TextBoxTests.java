c99180e057b7099f1021f7f233a23ed1a5cfe8f7
Артем Избышев <izbyshev-aa@mail.ru>
2026-05-04 19:21:44 +0300

    Добавил класс с конфигами TestBase

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -6,16 +6,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class TextBoxTests {
-    @BeforeAll
-    static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
-        Configuration.browser = "chrome";
-        Configuration.baseUrl = "https://demoqa.com";
-       // Configuration.pageLoadStrategy = "eager";
-        Configuration.timeout = 10000; // default = 4000;
-        Configuration.browserVersion = "144.0";
-    }
+public class TextBoxTests extends TestBase{
 
     @Test
 
