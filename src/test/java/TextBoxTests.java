5079bc8ee5919b94c1ea7a6e45502b6f4e87d3ef
Артем Избышев <izbyshev-aa@mail.ru>
2026-05-17 21:44:43 +0300

    Добавил PageObject

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Test;
+import pages.TextBoxPage;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
@@ -6,21 +7,33 @@ import static com.codeborne.selenide.Selenide.open;
 import static tests.testdata.TestData.*;
 
 public class TextBoxTests extends TestBase {
-
+    TextBoxPage textBoxPage = new TextBoxPage();
     @Test
 
     void successfulFillFormTest() {
-        open("/text-box");
-        $("[id=userName]").setValue(userName);
-        $("[id=userEmail]").setValue(userEmail);
-        $("[id=currentAddress]").setValue(currentAddress);
-        $("[id=permanentAddress]").setValue(permanentAddress);
-        $("[id=submit]").click();
 
 
-        $("[id=output] [id=name]").shouldHave(text(userName));
-        $("[id=output] [id=email]").shouldHave(text(userEmail));
-        $("[id=output] [id=currentAddress]").shouldHave(text(currentAddress));
-        $("[id=output] [id=permanentAddress]").shouldHave(text(permanentAddress));
+
+        textBoxPage.openPage();
+        textBoxPage.typeUserName(userName);
+        textBoxPage.typeUserEmail(userEmail);
+        textBoxPage.typeCurrentAddress(currentAddress);
+        textBoxPage.typePermanentAddress(permanentAddress);
+        textBoxPage.SubmitForm();
+        textBoxPage.checkField("name", userName);
+        textBoxPage.checkField("email", userEmail);
+        textBoxPage.checkField("currentAddress", currentAddress);
+        textBoxPage.checkField("permanentAddress", permanentAddress);
+
+    }
+
+    @Test
+    void sucessfulFillFormWithoutAdressTest() {
+        textBoxPage.openPage();
+        textBoxPage.typeUserName(userName);
+        textBoxPage.typeUserEmail(userEmail);
+        textBoxPage.SubmitForm();
+        textBoxPage.checkField("name", userName);
+        textBoxPage.checkField("name", userEmail);
     }
 }
