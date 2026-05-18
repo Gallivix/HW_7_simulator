8fab7fc8f91685174514b71886080ced295e4c6e
Артем Избышев <izbyshev-aa@mail.ru>
2026-05-19 00:48:16 +0300

    Добавил в домашку с позитивными тестами PageObject

--- src/test/java/ВарTestPracticeForm.java
+++ src/test/java/ВарTestPracticeForm.java
@@ -1,13 +1,49 @@
 import org.junit.jupiter.api.Test;
+import pages.RegistrationFormPage;
+//import pages.RegistrationFormPage;
 
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 import static tests.testdata.TestForm.*;
 
 public class ВарTestPracticeForm extends PracticeForm {
+
+    RegistrationFormPage registrationFormPage = new RegistrationFormPage();
+
+
     @Test
-    void StudentRegistrationForm() {
+    void StudentRegistrationForm_pageObject() {
+        registrationFormPage.openPage();
+        registrationFormPage.typeFirstName(firstName);
+        registrationFormPage.typeLastNameInput(lastName);
+        registrationFormPage.typeUserEmail(userEmail);
+        registrationFormPage.setGender();
+        registrationFormPage.typeUserNumberInput(userNumber);
+        registrationFormPage.typeSubjects();
+        registrationFormPage.typeSubjectsInput(subjectsInput);
+        registrationFormPage.typeSubjectsClick();
+        registrationFormPage.typeHobbiesCheckBox();
+        registrationFormPage.typeCurrentAddress(currentAddress);
+        registrationFormPage.state();
+        registrationFormPage.typeState();
+        registrationFormPage.city();
+        registrationFormPage.typeCity();
+        registrationFormPage.SubmitForm();
+        registrationFormPage.checkModalAppears();
+        registrationFormPage.checkResult(firstName);
+        registrationFormPage.checkResult(lastName);
+        registrationFormPage.checkResult(userEmail);
+        registrationFormPage.checkResult(userNumber);
+
+    }
+
+
+
+
+    @Test
+    void StudentRegistrationForm_old() {
         open("/automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
         $("[id=firstName]").setValue(firstName);
@@ -26,11 +62,11 @@ public class ВарTestPracticeForm extends PracticeForm {
         $("[id=react-select-4-option-1]").click();
         $("[id=submit]").click();
 
-        $("div.modal-dialog").should(appear);
+        $("div.modal-dialog").should( appear);
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").shouldHave(text("Artem"), text("Izbyshev"),
-                text("izbyshev-aa@mail.ru"),
-                text("9620352740"));
+        $(".table-responsive").shouldHave(text(firstName), text(lastName),
+                text(userEmail),
+                text(userNumber));
 
     }
 }
